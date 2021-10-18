72709248306b581c6fab078c4224830105c69712
vincentvvs <vincent.shi22@sitechhs.com>
2021-10-18 12:27:41 -0400

    ok
    ok

---
+++ hello-world/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <button @click="changeStudent">Change from Leyna to Eva</button>
+    <h1 class="APTest">Hello {{ student }}</h1>
+    <ul class="myList">
+      <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+    </ul>
+    <button v-if="isLoggedIn === false">Login</button>
+    <button v-else>Logout</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      student: "Leyna",
+      animals: ["Pig", "Cow", "Daniel", "Monkay", "Gibbon"],
+      isLoggedIn: true,
+    };
+  },
+  methods: {
+    changeStudent: function () {
+      this.student = "Eva";
+    },
+  },
+};
+</script>
+<style>
+.APTest {
+  color: red;
+}
+</style>
